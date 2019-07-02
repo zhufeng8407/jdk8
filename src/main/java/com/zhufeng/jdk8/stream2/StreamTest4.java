1143c0b86af83441b2132111696ae035942f7d20
zhufeng8407 <zhufeng.zhu@163.com>
2019-07-02 16:31:54 +0800

    jdk8 study

--- src/main/java/com/zhufeng/jdk8/stream2/StreamTest4.java
+++ src/main/java/com/zhufeng/jdk8/stream2/StreamTest4.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class StreamTest4 {
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("hello", "world", "hello world");
+        List<String> list = Arrays.asList("hello", "world", "hello world", "welcome", "person", "Student");
 
-        list.stream().forEach(System.out::println);
+//        list.stream().forEach(System.out::println);
+
+        list.parallelStream().forEach(System.out::println);
     }
 }
